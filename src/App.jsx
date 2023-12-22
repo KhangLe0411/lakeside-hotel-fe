993042b57c60c2ad930387a9a92267793abbcc28
KhangLe0411 <khangle04112002@gmail.com>
2023-12-22 13:10:23 +0700

    add RoomFilter, RoomPaginator, EditRoom, ExistingRooms

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,25 @@ import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '/node_modules/bootstrap/dist/js/bootstrap.min.js'
 import AddRoom from './components/room/AddRoom'
+import ExistingRooms from './components/room/ExistingRooms'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import EditRoom from './components/room/EditRoom'
+import Home from './components/home/Home'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <AddRoom />
+      <main>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/edit-room/:roomId' element={<EditRoom />} />
+            <Route path='/existing-rooms' element={<ExistingRooms />} />
+          </Routes>
+        </Router>
+      </main>
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
